1d6e770bd1b606781399a73433f36c06477d24ab
Lukáš Rada <rada@havit.cz>
2018-02-09 12:51:05 +0100

    Web: bring configuration back to ClientApp

---
+++ Web/ClientApp/apimodels/ValidationErrors.ts
@@ -0,0 +1,11 @@
+﻿/* tslint:disable */
+
+	export interface ValidationError {
+		errors: FieldValidationError[];
+		message: string;
+		statusCode: number;
+	}
+	export interface FieldValidationError {
+		field: string;
+		message: string;
+	}
--- Web/ClientApp/components/Home.tsx
+++ Web/ClientApp/components/Home.tsx
@@ -1,9 +1,16 @@
 ﻿import * as React from 'react'
 import Counter from '../containers/Counter'
+import Configuration from '../configuration'
 
 const Home = () => <div>
     <h1>NewProjectTemplate</h1>
     <Counter />
+    <h2>
+        Web configuration
+    </h2>
+    <code>
+        {JSON.stringify(Configuration)}
+    </code>
 </div>;
 
 export default Home
