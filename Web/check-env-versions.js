fd9961668750ba409dff8344a327c388b6d2487f
Pavel Kříž <kriz@havit.cz>
2018-02-21 15:27:40 +0100

    úpravy checkování verzí npm a node

--- Web/check-env-versions.js
+++ Web/check-env-versions.js
@@ -1,18 +1,16 @@
-﻿// const semver = require('semver');
-// const exec = require('child_process').exec;
-// const pkg = require('./package.json');
+﻿const semver = require('semver');
+const exec = require('child_process').exec;
+const pkg = require('./package.json');
 
-// const nodeVersion = pkg.engines.node;
-// const npmVersion = pkg.engines.npm;
+const nodeVersion = pkg.engines.node;
+const npmVersion = pkg.engines.npm;
 
-// if(!semver.satisfies(process.version, nodeVersion)) {
-//     console.error('Required node version' + nodeVersion + ' not satisfied with current version ' + process.version);
-//     process.exit(1);
-// }
+if (!semver.satisfies(process.version, nodeVersion)) {
+    console.error('Recommended node version ' + nodeVersion + ' not satisfied with current version ' + process.version);
+}
 
-// exec('npm -v', function(error, stdout, stderr) {
-//     if(!semver.satisfies(stdout, npmVersion)) {
-//         console.error('Required npm version' + npmVersion + ' not satisfied with current version ' + stdout);
-//         process.exit(1);
-//     }
-// });
+exec('npm -v', function (error, stdout, stderr) {
+    if (!semver.satisfies(stdout, npmVersion)) {
+        console.error('Recommended npm version ' + npmVersion + ' not satisfied with current version ' + stdout);
+    }
+});
