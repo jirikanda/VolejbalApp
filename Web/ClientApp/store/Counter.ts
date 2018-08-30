289b66ae92d5875a5ca2f7aa637a55ce02bc833c
Pavel Kříž <kriz@havit.cz>
2018-08-30 11:33:00 +0200

    40723 Přidat reducer factory a vytáhnout yarn příkazy z csproj do scriptů
    Related Work Items: #40723

--- Web/ClientApp/store/Counter.ts
+++ Web/ClientApp/store/Counter.ts
@@ -1,38 +1,41 @@
 ﻿import { Dispatch, Reducer } from 'redux';
 import { delay } from 'redux-saga';
 import { put, takeEvery, select } from 'redux-saga/effects';
-// -----------------
-// STATE - This defines the type of data maintained in the Redux store.
 
-export interface CounterState {
-    count: number;
-}
+import { createGenericReducer, Omit } from '../helpers/reducer-helper';
+
+// Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
+// declared type strings (and not any other arbitrary string).
+type ReducerKnownAction = IncrementCountAction | DecrementCountAction;
+
+// State
+type StateUnion = IncrementCountAction & DecrementCountAction;
+export type CounterState = Omit<StateUnion, "type">;
 
 // -----------------
 // ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
 // They do not themselves have any side-effects; they just describe something that is going to happen.
 // Use @typeName and isActionType for type detection that works even after serialization/deserialization.
 
-export enum CounterActionTypes {
-    IncrementCount = 'INCREMENT_COUNT',
-    DecrementCount = 'DECREMENT_COUNT',
+export enum CounterSagaActionTypes {
     IncrementCountAsync = 'INCREMENT_COUNT_ASYNC',
     DecrementCountAsync = 'DECREMENT_COUNT_ASYNC'
 }
 
+export enum CounterReducerActionTypes {
+    IncrementCount = 'INCREMENT_COUNT',
+    DecrementCount = 'DECREMENT_COUNT'
+}
+
 interface IncrementCountAction {
-    type: CounterActionTypes.IncrementCount;
+    type: CounterReducerActionTypes.IncrementCount;
     count: number;
 }
 
 interface DecrementCountAction {
-    type: CounterActionTypes.DecrementCount;
+    type: CounterReducerActionTypes.DecrementCount;
     count: number;
 }
-
-// Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
-// declared type strings (and not any other arbitrary string).
-type ReducerKnownAction = IncrementCountAction | DecrementCountAction;
 
 // Sagas
 function* incrementAsync() {
@@ -40,7 +43,7 @@ function* incrementAsync() {
     const counter: CounterState = state.counter;
 
     yield delay(1000)
-    yield put<ReducerKnownAction>({ type: CounterActionTypes.IncrementCount, count: counter.count + 1 })
+    yield put<ReducerKnownAction>({ type: CounterReducerActionTypes.IncrementCount, count: counter.count + 1 })
 }
 
 function* decrementAsync() {
@@ -48,17 +51,17 @@ function* decrementAsync() {
     const counter: CounterState = state.counter;
 
     yield delay(1000)
-    yield put<ReducerKnownAction>({ type: CounterActionTypes.DecrementCount, count: counter.count - 1 })
+    yield put<ReducerKnownAction>({ type: CounterReducerActionTypes.DecrementCount, count: counter.count - 1 })
 }
 
 export function* watchCounterAsync() {
-    yield takeEvery(CounterActionTypes.IncrementCountAsync, incrementAsync)
-    yield takeEvery(CounterActionTypes.DecrementCountAsync, decrementAsync)
+    yield takeEvery(CounterSagaActionTypes.IncrementCountAsync, incrementAsync)
+    yield takeEvery(CounterSagaActionTypes.DecrementCountAsync, decrementAsync)
 }
 
 export const actionDispatchers = (dispatch: Dispatch<ReducerKnownAction>) => ({
-    incrementAsync: () => { dispatch({ type: CounterActionTypes.IncrementCountAsync }) },
-    decrementAsync: () => { dispatch({ type: CounterActionTypes.DecrementCountAsync }) }
+    incrementAsync: () => { dispatch({ type: CounterSagaActionTypes.IncrementCountAsync }) },
+    decrementAsync: () => { dispatch({ type: CounterSagaActionTypes.DecrementCountAsync }) }
 })
 
 // ----------------
@@ -66,23 +69,4 @@ export const actionDispatchers = (dispatch: Dispatch<ReducerKnownAction>) => ({
 
 const unloadedState: CounterState = { count: 0 };
 
-export const reducer: Reducer<CounterState> = (state: CounterState, action: ReducerKnownAction) => {
-    switch (action.type) {
-        case CounterActionTypes.IncrementCount:
-            return {
-                ...state,
-                count: action.count
-            };
-        case CounterActionTypes.DecrementCount:
-            return {
-                ...state,
-                count: action.count
-            };
-        default:
-            const exhaustiveCheck: never = action;
-    }
-
-    // For unrecognized actions (or in cases where actions have no effect), must return the existing state
-    //  (or default initial state if none was supplied
-    return state || unloadedState;
-};
+export const reducer: Reducer<CounterState> = createGenericReducer<CounterState, ReducerKnownAction>(CounterReducerActionTypes, unloadedState);
