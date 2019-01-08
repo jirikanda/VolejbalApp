f36c740655402e60d19f0e202c041aea01d988c6
Kanda, Jiří <kanda@havit.cz>
2019-01-08 12:24:56 +0100

    CommonAssemblyInfo -> Directory.Build.props (pokračování)

--- ApiModelsGenerator/Properties/AssemblyInfo.cs
+++ ApiModelsGenerator/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("Havit.NewProjectTemplate.ApiModelsGenerator")]
-
 namespace Havit.NewProjectTemplate.ApiModelsGenerator.Properties
 {
     public class AssemblyInfo
