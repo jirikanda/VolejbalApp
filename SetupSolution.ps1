6e3cf90d421bbb0ca7335cf0aaba8c2e0ef0a5a6
Kanda, Jiří <kanda@havit.cz>
2019-01-15 15:37:57 +0100

    SetupSolution.ps1 - již neodebírá BOM z UTF-8

--- SetupSolution.ps1
+++ SetupSolution.ps1
@@ -1,4 +1,4 @@
-param (
+﻿param (
     [string]$NewSolutionName = "NewProjectTemplate",
 	[string]$NewSolutionCode = "999.XXX",
 	[string]$NewWebProjectPort = "9900",
@@ -12,7 +12,7 @@ param (
 
 [string]$SolutionFolder = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path);
 
-Get-ChildItem -recurse $SolutionFolder -include *.cs,*.csproj,*.sln,*.config,*.ps1,*.json,*.tsx,*.cshtml,*.props | where { $_ -is [System.IO.FileInfo] } | where { !$_.FullName.Contains("\packages\") } | where { !$_.FullName.Contains("\obj\") } | where { !$_.Name.Equals("_SetApplicationName.ps1") } |
+Get-ChildItem -recurse $SolutionFolder -include *.cs,*.csproj,*.config,*.ps1,*.json,*.tsx,*.cshtml,*.props | where { $_ -is [System.IO.FileInfo] } | where { !$_.FullName.Contains("\packages\") } | where { !$_.FullName.Contains("\obj\") } | where { !$_.FullName.Contains("package.json") } | where { !$_.Name.Equals("_SetApplicationName.ps1") } |
 Foreach-Object {
     Set-ItemProperty $_.FullName -name IsReadOnly -value $false
     [string]$Content = [IO.File]::ReadAllText($_.FullName)
@@ -20,7 +20,7 @@ Foreach-Object {
     $Content = $Content.Replace($OriginalSolutionCode, $NewSolutionCode)
     $Content = $Content.Replace($OriginalWebProjectPort, $NewWebProjectPort)
     $Content = $Content.Replace($OriginalWebAPIProjectCode, $NewWebAPIProjectCode)
-    [IO.File]::WriteAllText($_.FullName, $Content)
+    [IO.File]::WriteAllText($_.FullName, $Content, [System.Text.Encoding]::UTF8)
 }
 
 Rename-Item -path ([System.IO.Path]::Combine($SolutionFolder, $OriginalSolutionName + '.sln')) -newName ($NewSolutionName + '.sln')
