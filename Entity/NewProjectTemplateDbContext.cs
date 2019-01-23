cfc1e6331d566ffb1a618f2a5ead5924307fc1bb
Kanda, Jiří <kanda@havit.cz>
2019-01-23 17:15:18 +0100

    NewProjectTemplateDbContext - při Client Side Evaluation dotazu dojde k umyslné výjimce

--- Entity/NewProjectTemplateDbContext.cs
+++ Entity/NewProjectTemplateDbContext.cs
@@ -24,6 +24,15 @@ namespace Havit.NewProjectTemplate.Entity
 		}
 
 		/// <inheritdoc />
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+#if DEBUG
+			optionsBuilder.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+#endif
+		}
+
+		/// <inheritdoc />
 		protected override void CustomizeModelCreating(ModelBuilder modelBuilder)
 		{
 			base.CustomizeModelCreating(modelBuilder);
