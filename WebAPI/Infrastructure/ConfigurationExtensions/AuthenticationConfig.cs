785a460432b37703b8256666e7fc0465a7ab9fd3
Kanda, Jiří <kanda@havit.cz>
2019-01-23 15:58:15 +0100

    Doplnění OpenIdConnect do šablony (bez potřebného nuget balíčku)

--- WebAPI/Infrastructure/ConfigurationExtensions/AuthenticationConfig.cs
+++ WebAPI/Infrastructure/ConfigurationExtensions/AuthenticationConfig.cs
@@ -18,8 +18,9 @@ namespace Havit.NewProjectTemplate.WebAPI.Infrastructure.ConfigurationExtensions
         {
             services.Configure<Havit.NewProjectTemplate.WebAPI.Infrastructure.Security.JwtBearerSettings>(configuration.GetSection("AppSettings:JwtBearer"));
             JwtBearerSettings jwtBearerSettings = configuration.GetSection("AppSettings:JwtBearer").Get<Havit.NewProjectTemplate.WebAPI.Infrastructure.Security.JwtBearerSettings>();
+			//OpenIdConnectSettings openIdConnectSettings = configuration.GetSection("AppSettings:OpenIdConnect").Get<Havit.NewProjectTemplate.WebAPI.Infrastructure.Security.OpenIdConnectSettings>();
 
-            var authenticationBuilder = services.AddAuthentication(options => options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme);
+			var authenticationBuilder = services.AddAuthentication(options => options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme);
 
             authenticationBuilder = authenticationBuilder.AddJwtBearer(options =>
             {
@@ -29,24 +30,64 @@ namespace Havit.NewProjectTemplate.WebAPI.Infrastructure.ConfigurationExtensions
 
 			});
 
-            // Pod IClaimsTransformation je standardně zaregistrováno NoopClaimsTransformation
-            // Pokud přidáme naší vlastní službu přes Castle Windsor, je tato až druhá a není tak resolvována (uff),
-            // Proto tuto službu, kterou nechceme, odebereme (a použijeme službu zaregistrovanou přes Castle Windsor).
-            services.Remove(services.Where(item => item.ImplementationType == typeof(NoopClaimsTransformation)).Single());
+			//if (!String.IsNullOrEmpty(openIdConnectSettings.Authority))
+			//{
+			//	authenticationBuilder = authenticationBuilder
+			//		.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+			//		{
+			//			options.ResponseType = OpenIdConnectResponseType.Code;
+			//			options.CallbackPath = "/callback";
+			//			options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			//			options.Scope.Clear();
+
+			//			options.Authority = openIdConnectSettings.Authority;
+			//			options.ClientId = openIdConnectSettings.ClientId;
+			//			options.Scope.Add(openIdConnectSettings.Scope);
+			//			options.ClientSecret = openIdConnectSettings.ClientSecret;
+			//			options.RequireHttpsMetadata = false;
+			//		})
+			//		.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+			//		{
+			//			// Vestavěná metoda OnRedirectToLogin rozlišuje Ajaxové a ostatní requesty. Pro Ajaxové se vrací 401, pro ostatní 302 (Redirect). 
+			//			// Rozlišení requestů je dáno existencí hlavičky X-Requested-With, avšak ta se v cross domain requestech neposílá.
+			//			// Proto metodu nahrazujeme tak, aby vždy vracela 401.
+			//			options.Events.OnRedirectToLogin = context =>
+			//			{
+			//				context.Response.StatusCode = 401;
+			//				return Task.CompletedTask;
+			//			};
+			//			// Obdobně OnRedirectToAccessDenied (řeší situaci, kdy dojde k zamítnutí oprávnění v AuthorizeFilteru).
+			//			options.Events.OnRedirectToAccessDenied = context =>
+			//			{
+			//				context.Response.StatusCode = 403;
+			//				return Task.CompletedTask;
+			//			};
+			//		});
+			//}
+			// Pod IClaimsTransformation je standardně zaregistrováno NoopClaimsTransformation
+			// Pokud přidáme naší vlastní službu přes Castle Windsor, je tato až druhá a není tak resolvována (uff),
+			// Proto tuto službu, kterou nechceme, odebereme (a použijeme službu zaregistrovanou přes Castle Windsor).
+			services.Remove(services.Where(item => item.ImplementationType == typeof(NoopClaimsTransformation)).Single());
 
             addCustomizedAuthenticationCalled = true;
 
         }
         private static bool addCustomizedAuthenticationCalled = false;
 
-        public static string[] GetAuthenticationSchemes(IConfiguration configuration)
-        {
-            if (!addCustomizedAuthenticationCalled)
-            {
-                throw new InvalidOperationException("Nejdříve je třeba zavolat metodu AddCustomizedAuthentication.");
-            }
+		public static string[] GetAuthenticationSchemes(IConfiguration configuration)
+		{
+			if (!addCustomizedAuthenticationCalled)
+			{
+				throw new InvalidOperationException("Nejdříve je třeba zavolat metodu AddCustomizedAuthentication.");
+			}
 
-	        return new string[] { JwtBearerDefaults.AuthenticationScheme };
-        }
-    }
+			return new string[] { JwtBearerDefaults.AuthenticationScheme };
+
+			//OpenIdConnectSettings openIdConnectSettings = configuration.GetSection("AppSettings:OpenIdConnect").Get<Havit.NewProjectTemplate.WebAPI.Infrastructure.Security.OpenIdConnectSettings>();
+
+			//return String.IsNullOrEmpty(openIdConnectSettings.Authority)
+			//	? new string[] { JwtBearerDefaults.AuthenticationScheme }
+			//	: new string[] { JwtBearerDefaults.AuthenticationScheme, CookieAuthenticationDefaults.AuthenticationScheme };
+		}
+	}
 }
