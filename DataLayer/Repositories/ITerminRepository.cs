29a13554024fc2d5288eae6feaf28beb7f6bf3cd
Kanda, Jiří <kanda@havit.cz>
2019-02-18 10:38:59 +0100

    Vygenerování datalayeru

---
+++ DataLayer/Repositories/ITerminRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Data.Patterns.Repositories;
+using KandaEu.Volejbal.Model;
+
+namespace KandaEu.Volejbal.DataLayer.Repositories
+{
+	public partial interface ITerminRepository
+	{
+
+	}
+}
---
+++ DataLayer/Repositories/TerminDbRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Data.EntityFrameworkCore;
+using Havit.Data.EntityFrameworkCore.Patterns.Repositories;
+using Havit.Data.EntityFrameworkCore.Patterns.SoftDeletes;
+using Havit.Data.Patterns.DataEntries;
+using Havit.Data.Patterns.DataLoaders;
+using KandaEu.Volejbal.Model;
+
+namespace KandaEu.Volejbal.DataLayer.Repositories
+{
+	public partial class TerminDbRepository : ITerminRepository
+	{
+		
+	}
+}
