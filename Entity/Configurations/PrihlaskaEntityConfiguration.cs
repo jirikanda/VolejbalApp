c3d10f818bc8ea8ff23150244358dbaf962cb763
Kanda, Jiří <kanda@havit.cz>
2019-02-18 10:38:19 +0100

    Unikátní indexy

---
+++ Entity/Configurations/PrihlaskaEntityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using KandaEu.Volejbal.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KandaEu.Volejbal.Entity.Configurations
+{
+	public class PrihlaskaEntityConfiguration : IEntityTypeConfiguration<Prihlaska>
+	{
+		public void Configure(EntityTypeBuilder<Prihlaska> builder)
+		{
+			builder.HasIndex(prihlaska => new
+			{
+				prihlaska.TerminId,
+				prihlaska.OsobaId,
+				prihlaska.Deleted
+			})
+			.HasName("UIDX_Prihlaska_TerminId_OsobaId_Deleted")
+			.IsUnique()
+			.HasFilter(null);
+		}
+	}
+}
---
+++ Entity/Configurations/TerminEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using KandaEu.Volejbal.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KandaEu.Volejbal.Entity.Configurations
+{
+	public class TerminEntityConfiguration : IEntityTypeConfiguration<Termin>
+	{
+		public void Configure(EntityTypeBuilder<Termin> builder)
+		{
+			builder.HasIndex(termin => new { termin.Datum, termin.Deleted })
+				.HasName("UIDX_Termin_Datum_Deleted")
+				.IsUnique()
+				.HasFilter(null);
+		}
+	}
+}
