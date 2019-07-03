25d602bad9f4a643a75e747f5b5b961d94847664
Kanda, Jiří <kanda@havit.cz>
2019-07-03 16:14:00 +0200

    Hrátky s Blazor

---
+++ Blazor/Startup.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+
+namespace Blazor
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//services.Configure<RequestLocalizationOptions>(options =>
+			//{
+			//	options.DefaultRequestCulture = new RequestCulture("en-US");
+			//});
+
+		}
+
+		public void Configure(IComponentsApplicationBuilder app)
+		{
+			app.AddComponent<App>("app");
+
+			var cultureInfo = new CultureInfo("cs-CZ");
+			CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+			CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+			//app.UseRequestLocalization();
+		}
+	}
+}
