e271a4e0e2b0c01cc0bc22c51f47ffeccc27dd63
Kanda, Jiří <kanda@havit.cz>
2019-09-20 15:27:03 +0200

    Zavedení code-behind

---
+++ Web/Components/Prihlasovani/PrihlasovaniBase.cs
@@ -0,0 +1,61 @@
+﻿using KandaEu.Volejbal.Web.Components.Terminy;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KandaEu.Volejbal.Web.Components.Prihlasovani
+{
+	public class PrihlasovaniBase : ComponentBase, EventAggregator.Blazor.IHandle<KandaEu.Volejbal.Web.Components.Terminy.CurrentTerminChanged>
+	{
+		[Inject]
+		public IHttpClientFactory HttpClientFactory { get; set; }
+
+		[Inject]
+		public EventAggregator.Blazor.IEventAggregator EventAggregator { get; set; }
+
+		protected PrihlasovaniState State { get; } = new PrihlasovaniState();
+
+		protected override void OnInitialized()
+		{
+			base.OnInitialized();
+			EventAggregator.Subscribe(this);
+		}
+
+		public async Task SetCurrentTermin(int terminId)
+		{
+			State.AktualniTerminId = terminId;
+
+			State.IsLoading = true;
+			State.Prihlaseni = null;
+			State.Neprihlaseni = null;
+
+			System.Threading.Thread.Sleep(1000);
+
+			try
+			{
+				var terminDetail = await HttpClientFactory.CreateClient().GetJsonAsync<KandaEu.Volejbal.Contracts.Terminy.Dto.TerminDetailDto>($"http://localhost:9901/api/terminy/{terminId}");
+
+				State.Prihlaseni = terminDetail.Prihlaseni;
+				State.Neprihlaseni = terminDetail.Neprihlaseni;
+			}
+			catch
+			{
+				State.LoadingFailed = true;
+			}
+			finally
+			{
+				State.IsLoading = false;
+			}
+
+			StateHasChanged();
+		}
+
+		async Task EventAggregator.Blazor.IHandle<KandaEu.Volejbal.Web.Components.Terminy.CurrentTerminChanged>.HandleAsync(CurrentTerminChanged message)
+		{
+			await SetCurrentTermin(message.TerminId);
+		}
+	}
+}
---
+++ Web/Components/Terminy/TerminyBase.cs
@@ -0,0 +1,60 @@
+﻿using EventAggregator.Blazor;
+using KandaEu.Volejbal.Contracts.Terminy.Dto;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KandaEu.Volejbal.Web.Components.Terminy
+{
+	public class TerminyBase : ComponentBase
+	{
+		[Inject]
+		public IHttpClientFactory HttpClientFactory { get; set; }
+		
+		[Inject]
+		public IEventAggregator EventAggregator { get; set; }
+
+		protected TerminyState State { get; } = new TerminyState();
+
+		protected override async Task OnInitializedAsync()
+		{
+			await base.OnInitializedAsync();
+
+			State.IsLoading = true;
+
+			TerminListDto terminList;
+			try
+			{
+
+				terminList = await HttpClientFactory.CreateClient().GetJsonAsync<TerminListDto>("http://localhost:9901/api/terminy");
+				await Task.Delay(1000);
+			}
+			catch
+			{
+				State.LoadingFailed = true;
+				throw; // ???
+			}
+			finally
+			{
+				State.IsLoading = false;
+			}
+			State.Terminy = terminList.Terminy;
+			if (State.Terminy.Count > 0)
+			{
+				await EventAggregator.PublishAsync(new CurrentTerminChanged(State.Terminy[0].Id));
+			}
+		}
+
+		protected Func<MouseEventArgs, Task> XButtonClick(int terminId)
+		{
+			return async (MouseEventArgs e) =>
+			{
+				await EventAggregator.PublishAsync(new CurrentTerminChanged(terminId));
+			};
+		}
+	}
+}
