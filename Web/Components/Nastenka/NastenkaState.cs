b038bbfdd4d04512d28762a3a950b997120ad0a8
Kanda, Jiří <kanda@havit.cz>
2019-10-04 15:49:05 +0200

    Kosmetika (zakomentování nepoužívané vlasntosti)

--- Web/Components/Nastenka/NastenkaState.cs
+++ Web/Components/Nastenka/NastenkaState.cs
@@ -1,6 +1,4 @@
-﻿using Contracts.Nastenka.Dto;
-using KandaEu.Volejbal.Contracts.Terminy.Dto;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +9,6 @@ namespace KandaEu.Volejbal.Web.Components.Nastenka
 	{
 		public bool IsLoading { get; set; }
 		public bool LoadingFailed { get; set; }
-		public List<VzkazDto> Vzkazy { get; set; }
+		//public List<VzkazDto> Vzkazy { get; set; }
 	}
 }
