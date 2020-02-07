acc1551cbec1e0502b6e6d825c14866a9df0378e
Kanda, Jiří <kanda@havit.cz>
2020-02-07 17:25:56 +0100

    Web - Loading komponenta

---
+++ Web/Components/LoadingComponent/LoadingState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KandaEu.Volejbal.Web.Components.LoadingComponent
+{
+	public class LoadingState
+	{
+		public bool LoadingInProgress { get; set; }
+		public bool LoadingFailed { get; set; }
+	}
+}
