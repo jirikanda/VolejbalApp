0fcd9617c21d2f0d953c573249e8b266bc899c24
Kanda, Jiří <kanda@havit.cz>
2020-02-12 11:12:19 +0100

    Nástěnka na homepage (draft funkcionality)

---
+++ Web/Pages/Index.razor.cs
@@ -0,0 +1,24 @@
+﻿using KandaEu.Volejbal.Web.WebApiClients;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KandaEu.Volejbal.Web.Pages
+{
+	public partial class Index
+	{
+		[Inject]
+		INastenkaWebApiClient NastenkaWebApiClient { get; set; }
+
+		protected bool ShowNastenkaLink { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			await base.OnInitializedAsync();
+			ShowNastenkaLink = (await NastenkaWebApiClient.GetVzkazyAsync()).Vzkazy.Any();
+		}
+
+	}
+}
