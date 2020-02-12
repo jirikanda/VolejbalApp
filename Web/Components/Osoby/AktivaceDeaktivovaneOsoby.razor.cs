459ba460c4df3f360f0669788893e1cca623883c
Kanda, Jiří <kanda@havit.cz>
2020-02-12 16:27:59 +0100

    Správa osob (základ)

---
+++ Web/Components/Osoby/AktivaceDeaktivovaneOsoby.razor.cs
@@ -0,0 +1,40 @@
+﻿using KandaEu.Volejbal.Web.Components.ProgressComponent;
+using KandaEu.Volejbal.Web.WebApiClients;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KandaEu.Volejbal.Web.Components.Osoby
+{
+	public partial class AktivaceDeaktivovaneOsoby
+	{
+		[Inject]
+		public IOsobaWebApiClient OsobaWebApiClient { get; set; }
+
+		[CascadingParameter]
+		private Progress Progress { get; set; }
+
+		OsobaListDto osoby;
+
+		protected override async Task OnInitializedAsync()
+		{
+			await base.OnInitializedAsync();
+			osoby = await Progress.ExecuteInProgressAsync(async () => await OsobaWebApiClient.GetNeaktivniOsobyAsync());
+		}
+
+		private async Task Aktivovat(OsobaDto2 osoba)
+		{
+			// TODO: TOASTER
+			await Progress.ExecuteInProgressAsync(async () => await OsobaWebApiClient.AktivujNeaktivniOsobuAsync(osoba.Id));
+			osoby.Osoby.Remove(osoba);
+		}
+
+		private async Task Smazat(OsobaDto2 osoba)
+		{
+			// TODO: TOASTER
+			await Progress.ExecuteInProgressAsync(async () => await OsobaWebApiClient.SmazNeaktivniOsobuAsync(osoba.Id));
+			osoby.Osoby.Remove(osoba);
+		}
+	}
+}
---
+++ Web/Components/Osoby/ZalozeniNoveOsoby.razor.cs
@@ -0,0 +1,36 @@
+﻿using KandaEu.Volejbal.Web.Components.ProgressComponent;
+using KandaEu.Volejbal.Web.WebApiClients;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KandaEu.Volejbal.Web.Components.Osoby
+{
+	public partial class ZalozeniNoveOsoby
+	{
+		[Inject]
+		public IOsobaWebApiClient OsobaWebApiClient { get; set; }
+
+		[Inject]
+		public NavigationManager NavigationManager { get; set; }
+
+		[CascadingParameter]
+		public Progress Progress { get; set; }
+
+		public NovaOsobaFormData formData = new NovaOsobaFormData();
+		
+		private async Task ValidSubmit()
+		{
+			OsobaInputDto novaOsoba = new OsobaInputDto()
+			{
+				Jmeno = formData.Jmeno,
+				Prijmeni = formData.Prijmeni,
+				Email = formData.Email
+			};
+
+			await Progress.ExecuteInProgressAsync(async () => await OsobaWebApiClient.VlozOsobuAsync(novaOsoba));
+			NavigationManager.NavigateTo("/");
+		}
+	}
+}
