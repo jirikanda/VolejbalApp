86af92a5b88ae6cc99e5fd92d3f30bbccd75c338
Kanda, Jiří <kanda@havit.cz>
2020-03-10 14:38:46 +0100

    Blazor day (to be reverted)

---
+++ BlazorDay.WebApiClients/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using BlazorDay.WebApiClients;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static void AddWebApiClients(this IServiceCollection services, string baseUrl = "https://blazorday-api.azurewebsites.net")
+		{
+			services.AddHttpClient();
+			services.AddHttpClient<ISystemWebApiClient, SystemWebApiClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl));
+			services.AddHttpClient<ITerminWebApiClient, TerminWebApiClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl));
+			services.AddHttpClient<IOsobaWebApiClient, OsobaWebApiClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl));
+			services.AddHttpClient<INastenkaWebApiClient, NastenkaWebApiClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl));
+			services.AddHttpClient<IReportWebApiClient, ReportWebApiClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(baseUrl));
+		}
+	}
+}
--- WebAPI/Infrastructure/ConfigurationExtensions/OpenApiConfig.cs
+++ WebAPI/Infrastructure/ConfigurationExtensions/OpenApiConfig.cs
@@ -12,7 +12,7 @@ namespace KandaEu.Volejbal.WebAPI.Infrastructure.ConfigurationExtensions
 			services.AddOpenApiDocument(c =>
 			{
 				c.DocumentName = "current";
-				c.Title = "VolejbalApp";
+				c.Title = "BlazorDayApi";
 				c.Version = System.Diagnostics.FileVersionInfo.GetVersionInfo(typeof(KandaEu.Volejbal.WebAPI.Properties.AssemblyInfo).Assembly.Location).ProductVersion;				
 			});
 		}
