8312c8d041ec2efab4187819b9c6bd99c75662ff
Kanda, Jiří <kanda@havit.cz>
2022-03-07 09:51:36 +0100

    Error Boundary

---
+++ Web/Components/Error.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KandaEu.Volejbal.Web.Components
+{
+    public partial class Error
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        private void HandleRestartClick()
+        {
+            NavigationManager.NavigateTo("/", forceLoad: true);
+        }
+    }
+}
