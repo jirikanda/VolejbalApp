80c4f4d869adae45f3be98f56833c64877619bb4
Jiří Kanda <kanda@havit.cz>
2024-02-13 11:48:25 +0100

    Prerendering + Terminy prerendered state persistence

--- Web/Components/Pages/Terminy/Terminy.razor.cs
+++ Web/Components/Pages/Terminy/Terminy.razor.cs
@@ -1,44 +1,68 @@
-﻿using KandaEu.Volejbal.Contracts.Terminy.Dto;
+﻿using System;
+using KandaEu.Volejbal.Contracts.Terminy.Dto;
 
 // TODO: Není page!
 namespace KandaEu.Volejbal.Web.Components.Pages.Terminy;
 
-public partial class Terminy
+public partial class Terminy : IDisposable
 {
-	[Inject]
-	protected ITerminWebApiClient TerminWebApiClient { get; set; }
+	[Inject] protected ITerminWebApiClient TerminWebApiClient { get; set; }
 
-	protected TerminyState State { get; } = new TerminyState();
+	[Inject] protected  PersistentComponentState ApplicationState { get; set; }
 
-	[CascadingParameter]
-	protected ProgressComponent.Progress Progress { get; set; }
+	[CascadingParameter] protected ProgressComponent.Progress Progress { get; set; }
+
+	protected TerminyState State { get; set; } = new TerminyState();
 
 	[Parameter] public EventCallback<int> CurrentTerminIdChanged { get; set; }
+
+	private PersistingComponentStateSubscription _persistingSubscription;
 
 	protected override async Task OnInitializedAsync()
 	{
 		await base.OnInitializedAsync();
 
-		TerminListDto terminList = await Progress.ExecuteInProgressAsync(async () => await TerminWebApiClient.GetTerminyAsync());
+		_persistingSubscription = ApplicationState.RegisterOnPersisting(PersistDataAsync);
 
-		State.Terminy = terminList.Terminy.ToList();
-
-		StateHasChanged();
-
-		if (State.Terminy.Count > 0)
+		if (ApplicationState.TryTakeFromJson<TerminyState>("State", out TerminyState restoredState))
 		{
-			await SetCurrentTerminAsync(State.Terminy[0]);
+			State = restoredState;
+			if (State.CurrentTerminId != null)
+			{
+				await SetCurrentTerminIdAsync(State.CurrentTerminId.Value);
+			}
+		}
+		else
+		{
+			TerminListDto terminList = await Progress.ExecuteInProgressAsync(async () => await TerminWebApiClient.GetTerminyAsync());
+			State.Terminy = terminList.Terminy.ToList();
+
+			if (State.Terminy.Count > 0)
+			{
+				await SetCurrentTerminIdAsync(State.Terminy[0].Id);
+			}
 		}
 	}
 
 	protected async Task TerminClickAsync(TerminDto termin)
 	{
-		await SetCurrentTerminAsync(termin);
+		await SetCurrentTerminIdAsync(termin.Id);
 	}
 
-	private async Task SetCurrentTerminAsync(TerminDto termin)
+	private async Task SetCurrentTerminIdAsync(int terminId)
 	{
-		State.CurrentTerminId = termin.Id;
-		await CurrentTerminIdChanged.InvokeAsync(termin.Id);
+		State.CurrentTerminId = terminId;
+		await CurrentTerminIdChanged.InvokeAsync(terminId);
+	}
+
+	private Task PersistDataAsync()
+	{
+		ApplicationState.PersistAsJson("State", State);
+		return Task.CompletedTask;
+	}
+
+	public void Dispose()
+	{
+		_persistingSubscription.Dispose();
 	}
 }
