d9bdc7d4fb9463e9febfc6d8104e3a936ecfa1f7
Jiří Kanda <kanda@havit.cz>
2026-05-17 12:43:01 +0200

    Redesign ladění

--- Web/Components/Pages/Terminy/Terminy.razor.cs
+++ Web/Components/Pages/Terminy/Terminy.razor.cs
@@ -64,21 +64,4 @@ public partial class Terminy : IDisposable
 	{
 		_persistingSubscription.Dispose();
 	}
-
-	private static string CzechMonth(int month) => month switch
-	{
-		1 => "leden",
-		2 => "únor",
-		3 => "březen",
-		4 => "duben",
-		5 => "květen",
-		6 => "červen",
-		7 => "červenec",
-		8 => "srpen",
-		9 => "září",
-		10 => "říjen",
-		11 => "listopad",
-		12 => "prosinec",
-		_ => string.Empty
-	};
 }
